4c00e8aa3899a053fafa6643095ad92fffbda40f
santiagofacchini <santiago.facchini@icloud.com>
2022-11-13 23:24:02 -0300

    Added exception handling when switch is unreachable

--- services/SonoffClient.py
+++ services/SonoffClient.py
@@ -13,71 +13,97 @@ class Switch:
         self.sonoff_port = sonoff_port
 
     def get_info(self):
-        response = requests.post(
-            url=f'http://{self.sonoff_ip}:{self.sonoff_port}/zeroconf/info',
-            json={
-                "data": {}
-            },
-        )
-        return response.json()
+        try:
+            response = requests.post(
+                url=f'http://{self.sonoff_ip}:{self.sonoff_port}/zeroconf/info',
+                json={
+                    "data": {}
+                },
+            )
+            return response.json()
+        except:
+            print(f'http://{self.sonoff_ip}:{self.sonoff_port} unreachable')
+            return None
+
 
     def turn_on(self):
-        response = requests.post(
-            url=f'http://{self.sonoff_ip}:{self.sonoff_port}/zeroconf/switch',
-            json={
-                "data": {
-                    "switch": "on"
-                }
-            },
-        )
-        print(f'{datetime.datetime.now()} => Turning switch at '
-            f'{self.sonoff_ip} on. {response}')
-        return response.json()
+        try:
+            response = requests.post(
+                url=f'http://{self.sonoff_ip}:{self.sonoff_port}/zeroconf/switch',
+                json={
+                    "data": {
+                        "switch": "on"
+                    }
+                },
+            )
+            print(f'{datetime.datetime.now()} => Turning switch at '
+                f'{self.sonoff_ip} on. {response}')
+            return response.json()
+        except:
+            print(f'http://{self.sonoff_ip}:{self.sonoff_port} unreachable')
+            return None
 
     def turn_off(self):
-        response = requests.post(
-            url=f'http://{self.sonoff_ip}:{self.sonoff_port}/zeroconf/switch',
-            json={
-                "data": {
-                    "switch": "off"
-                }
-            },
-        )
-        print(f'{datetime.datetime.now()} => Turning switch at '
-            f'{self.sonoff_ip} off. {response}')
-        return response.json()
+        try:
+            response = requests.post(
+                url=f'http://{self.sonoff_ip}:{self.sonoff_port}/zeroconf/switch',
+                json={
+                    "data": {
+                        "switch": "off"
+                    }
+                },
+            )
+            print(f'{datetime.datetime.now()} => Turning switch at '
+                f'{self.sonoff_ip} off. {response}')
+            return response.json()
+        except:
+            print(f'http://{self.sonoff_ip}:{self.sonoff_port} unreachable')
+            return None
 
     def inching_on(self, time:int):
         miliseconds = time*60000
-        response = requests.post(
-            url=f'http://{self.sonoff_ip}:{self.sonoff_port}/zeroconf/pulse',
-            json={
-                "data": {
-                    "pulse": "on",
-                    "pulseWidth": miliseconds,
-                }
-            },
-        )
-        print(f'{datetime.datetime.now()} => Inching set to '
-            f'{miliseconds}. {response}')
-        return response.json()
+        try:
+            response = requests.post(
+                url=f'http://{self.sonoff_ip}:{self.sonoff_port}/zeroconf/pulse',
+                json={
+                    "data": {
+                        "pulse": "on",
+                        "pulseWidth": miliseconds,
+                    }
+                },
+            )
+            print(f'{datetime.datetime.now()} => Inching set to '
+                f'{miliseconds}. {response}')
+            return response.json()
+        except:
+            print(f'http://{self.sonoff_ip}:{self.sonoff_port} unreachable')
+            return None
 
     def inching_off(self):
-        response = requests.post(
-            url=f'http://{self.sonoff_ip}:{self.sonoff_port}/zeroconf/pulse',
-            json={
-                "data": {
-                    "pulse": "off",
-                }
-            },
-        )
-        print(f'{datetime.datetime.now()} => Inching off. {response}')
-        return response.json()
+        try:
+            response = requests.post(
+                url=f'http://{self.sonoff_ip}:{self.sonoff_port}/zeroconf/pulse',
+                json={
+                    "data": {
+                        "pulse": "off",
+                    }
+                },
+            )
+            print(f'{datetime.datetime.now()} => Inching off. {response}')
+            return response.json()
+        except:
+            print(f'http://{self.sonoff_ip}:{self.sonoff_port} unreachable')
+            return None
 
     def switch_state(self):
-        sprinklers_state = self.get_info()
-        if sprinklers_state['data']['switch'] == 'off':
-            self.turn_on()
-        else:
-            self.turn_off()
-        return self.get_info()
+        try:
+            sprinklers_state = self.get_info()
+            if sprinklers_state['data']['switch'] == 'off':
+                self.turn_on()
+            else:
+                self.turn_off()
+            return self.get_info()
+        except:
+            print(f'http://{self.sonoff_ip}:{self.sonoff_port} unreachable')
+            return None
+
